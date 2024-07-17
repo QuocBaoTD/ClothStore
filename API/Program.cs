ef9c45ee064f0a8dbcac4cc395cbc33f920be905
QuocBaoTD <tdpbao1998@gmail.com>
2024-07-17 22:46:50 +0700

    Build basic Api

---
+++ API/Program.cs
@@ -0,0 +1,45 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opts =>
+{
+    opts.UseSqlite(builder.Configuration.GetConnectionString("StoreConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+try
+{
+    context.Database.Migrate();
+    DbInitial.Initialize(context);
+}
+catch (Exception ex)
+{
+
+    logger.LogError(ex, "A problem ocurred during migration");
+}
+
+app.MapControllers();
+
+app.Run();
